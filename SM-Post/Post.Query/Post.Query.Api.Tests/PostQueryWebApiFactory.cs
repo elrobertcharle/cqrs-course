2e05d71b4b036e5927d4091845968fa42ef33942
Roberto C. Castaño <elrobertcharle@outlook.com>
2025-06-23 11:58:48 -0400

    added Microsoft.EntityFrameworkCore pkg to tests

--- SM-Post/Post.Query/Post.Query.Api.Tests/PostQueryWebApiFactory.cs
+++ SM-Post/Post.Query/Post.Query.Api.Tests/PostQueryWebApiFactory.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.VisualStudio.TestPlatform.TestHost;
