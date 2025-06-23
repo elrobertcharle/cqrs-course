ea64fc2ea6fd84b0a471379cdcb0d5e41dd463e7
Roberto C. Castaño <elrobertcharle@outlook.com>
2025-06-23 12:25:29 -0400

    added test GetByAuthor_Works

--- SM-Post/Post.Query/Post.Query.Api.Tests/Tests/PostLookupControllerTests.cs
+++ SM-Post/Post.Query/Post.Query.Api.Tests/Tests/PostLookupControllerTests.cs
@@ -48,6 +48,7 @@ namespace Post.Query.Api.Tests.Tests
                     Comments = new HashSet<CommentEntity>()
                 };
 
+                dbContext.RemoveRange(dbContext.Posts);
                 dbContext.Posts.Add(expectedPost);
                 await dbContext.SaveChangesAsync();
 
@@ -59,6 +60,57 @@ namespace Post.Query.Api.Tests.Tests
                 content.Should().HaveCount(1);
                 var actualPost = content[0];
                 actualPost.Should().BeEquivalentTo(expectedPost, options => options
+                    .Using<DateTime>(ac => ac.Subject.Should().BeCloseTo(ac.Expectation, 1.Seconds()))
+                    .WhenTypeIs<DateTime>());
+            }
+        }
+
+        [Fact]
+        public async Task GetByAuthor_Works()
+        {
+            using (var scope = _appFactory.Services.CreateScope())
+            {
+                var scopedServices = scope.ServiceProvider;
+                var dbContext = scopedServices.GetRequiredService<DatabaseContext>();
+                var commonAuthor = "Pepe Martinez";
+
+                List<PostEntity> posts = [new PostEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Author = commonAuthor,
+                    CreatedDate = DateTime.UtcNow,
+                    Likes = 1,
+                    Message = "This is the post text 1.",
+                    Comments = new HashSet<CommentEntity>()
+                },new PostEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Author = commonAuthor,
+                    CreatedDate = DateTime.UtcNow,
+                    Likes = 1,
+                    Message = "This is the post text 2.",
+                    Comments = new HashSet<CommentEntity>()
+                },new PostEntity
+                {
+                    Id = Guid.NewGuid(),
+                    Author = "Jon Jones",
+                    CreatedDate = DateTime.UtcNow,
+                    Likes = 1,
+                    Message = "This is the post text 3.",
+                    Comments = new HashSet<CommentEntity>()
+                }];
+
+                dbContext.RemoveRange(dbContext.Posts);
+                dbContext.Posts.AddRange(posts);
+                await dbContext.SaveChangesAsync();
+
+                var response = await _client.GetAsync($"{_controllerUrl}/by-author?author={commonAuthor}");
+                response.IsSuccessStatusCode.Should().BeTrue();
+                var actualPosts = await response.Content.ReadFromJsonAsync<List<PostEntity>>();
+                actualPosts.Should().NotBeNull();
+                actualPosts.Should().HaveCount(2);
+                var expectedPosts = posts.Take(2);
+                actualPosts.Should().BeEquivalentTo(expectedPosts, options => options
                     .Using<DateTime>(ac => ac.Subject.Should().BeCloseTo(ac.Expectation, 1.Seconds()))
                     .WhenTypeIs<DateTime>());
             }
